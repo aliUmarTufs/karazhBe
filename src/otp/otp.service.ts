3d6cee365ba54e1fcd495bb1d04ad097b830184c
saad <saadalam803@gmail.com>
2024-09-06 22:32:54 +0500

    fix: otp verification api

--- src/otp/otp.service.ts
+++ src/otp/otp.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as otpGenerator from 'otp-generator';
 import { MailerService } from '../mailer/mailer.service';
@@ -6,58 +11,80 @@ import { addMinutes } from 'date-fns';
 
 @Injectable()
 export class OtpService {
+  private readonly logger = new Logger(OtpService.name);
+
   constructor(
     private prisma: PrismaService,
     private mailerService: MailerService,
   ) {}
 
   async generateOtp(email: string) {
-    const otp = otpGenerator.generate(4, {
-      upperCase: false,
-      specialChars: false,
-    });
-    const expiresAt = addMinutes(new Date(), 10); // OTP expires in 10 minutes
+    this.logger.log(
+      `${this.generateOtp.name} has been called | email: ${email}`,
+    );
+    try {
+      const otp = otpGenerator.generate(4, {
+        upperCase: false,
+        specialChars: false,
+      });
+      const expiresAt = addMinutes(new Date(), 10); // OTP expires in 10 minutes
 
-    const user = await this.prisma.user.findUnique({ where: { email } });
-    if (!user) {
-      throw new UnauthorizedException('User not found');
+      const user = await this.prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+
+      await this.prisma.otp.create({
+        data: {
+          token: otp,
+          userId: user.id,
+          expiresAt,
+        },
+      });
+
+      // Send OTP via email
+      await this.mailerService.sendOtpEmail(email, otp);
+
+      return { message: 'OTP has been sent to your email' };
+    } catch (error) {
+      this.logger.error(
+        `${this.generateOtp.name} got an Error: ${JSON.stringify(error)}`,
+      );
+      throw new BadRequestException(error.message);
     }
-
-    await this.prisma.otp.create({
-      data: {
-        token: otp,
-        userId: user.id,
-        expiresAt,
-      },
-    });
-
-    // Send OTP via email
-    await this.mailerService.sendOtpEmail(email, otp);
-
-    return { message: 'OTP has been sent to your email' };
   }
 
   async verifyOtp(email: string, token: string): Promise<boolean> {
-    const user = await this.prisma.user.findUnique({ where: { email } });
-    if (!user) {
-      throw new UnauthorizedException('User not found');
-    }
+    this.logger.log(
+      `${this.verifyOtp.name} has been called | email: ${email}, token: ${token}`,
+    );
+    try {
+      const user = await this.prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
 
-    const otpRecord = await this.prisma.otp.findFirst({
-      where: {
-        userId: user.id,
-        token,
-        expiresAt: { gte: new Date() },
-      },
-    });
+      const otpRecord = await this.prisma.otp.findFirst({
+        where: {
+          userId: user.id,
+          token,
+          expiresAt: { gte: new Date() },
+        },
+      });
 
-    if (!otpRecord) {
+      if (!otpRecord) {
+        return false;
+      }
+
+      // Optionally, delete the OTP record after successful verification
+      await this.prisma.otp.delete({ where: { id: otpRecord?.id } });
+
+      return true;
+    } catch (error) {
+      this.logger.error(
+        `${this.verifyOtp.name} got an Error: ${JSON.stringify(error)}`,
+      );
       return false;
     }
-
-    // Optionally, delete the OTP record after successful verification
-    await this.prisma.otp.delete({ where: { id: otpRecord.id } });
-
-    return true;
   }
 }
